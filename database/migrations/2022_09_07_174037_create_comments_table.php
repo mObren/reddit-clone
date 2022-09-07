3ac55fb3d76610cd49fedca8b2363907209d8e7e
mObren <mobrenovic040@gmail.com>
2022-09-07 20:01:57 +0200

    Adding migrations, models and relations

--- database/migrations/2022_09_07_174037_create_comments_table.php
+++ database/migrations/2022_09_07_174037_create_comments_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->integer('commentable_id')->unsigned();
+            $table->string('commentable_type');
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('comments');
     }
 };
